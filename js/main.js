db730b30599b08830274b46c5ccf1cd9bbaffdc6
Michael <mikeed1998@gmail.com>
2022-10-20 16:25:46 -0500

    responsive avances 3

--- js/main.js
+++ js/main.js
@@ -3,30 +3,30 @@ $(document).ready(function() {
         slidesToShow: 4,
         slidesToScroll: 1,
         nextArrow: '#btn-slick-2',
-      responsive: [
-      {
-          breakpoint: 1024,
-          settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-          }
-      },
-      {
-          breakpoint: 600,
-          settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
-          }
-      },
-      {
-          breakpoint: 480,
-          settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-          }
-      }
-      ] 
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+        }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        ] 
     });
 });
